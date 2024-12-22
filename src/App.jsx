7b3d658473e08adf74aae68fd66da9d94925641e
pavanydg <pavanyermunja@gmail.com>
2024-12-23 04:28:29 +0530

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <Routes>
-        <Route path='/signin' element={<SignInPage/>} />
+        <Route path='/' element={<SignInPage/>} />
         <Route path='/dashboard' element={<DashBoard/>} />
     </Routes>
   )
